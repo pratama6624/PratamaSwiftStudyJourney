c7224ba2a74162d2baf22759a0e10bc6a16059cc
pratama6624 <scyoung6624@gmail.com>
2025-05-21 08:25:28 +0700

    Closures
    - Escaping Closures
    
    - Escaping Closures Sample 1

--- Sources/SwiftRoute/Closures/PartThree.swift
+++ Sources/SwiftRoute/Closures/PartThree.swift
@@ -46,4 +46,72 @@ print("Counter B : \(counterB())") // Leading to the same closure
 print("Counter C : \(counterC())") // Leading to the same closure
 print("Counter B : \(counterB())") // Leading to the same closure
 
-// Escaping Closures
+// Escaping
+// Nonescaping Closure
+func sayHelloNow(closure: () -> Void) {
+    closure()
+}
+sayHelloNow {
+    print("Hi bro, just answer here")    
+}
+// Escaping Closure
+// Sample 1
+var tasks: [() -> Void] = []
+func sayHelloLater(closure: @escaping () -> Void) {
+    tasks.append(closure)
+}
+sayHelloLater {
+    print("Relax bro, you can answer later")
+}
+tasks.first?()
+// Escaping sample -> real case
+let semaphore = DispatchSemaphore(value: 0)
+struct User: Decodable {
+    let name: String
+    let email: String
+}
+// Netwoking Manager
+class NetworkManager {
+    func fetchUser(completion: @escaping (User?) -> Void) {
+        // API Delay Simulation
+        DispatchQueue.global().asyncAfter(deadline: .now() + 2) {
+            // Data simulation successfully taken
+            let dummyUser = User(name: "Tama", email: "tama@gmail.com")
+            // When finished, call the closure
+            completion(dummyUser)
+            semaphore.signal()
+        }
+    }
+}
+// Use -> Network Manager
+let network = NetworkManager()
+print("⏳ Menunggu respon dari API...")
+network.fetchUser { user in
+    if let user = user {
+        print("Nama user: \(user.name)")
+        print("Email user: \(user.email)")
+    } else {
+        print("Gagal ambil data.")
+    }
+}
+semaphore.wait()
+// Sample 2
+var completionHandlers: [() -> Void] = []
+let semaphore1 = DispatchSemaphore(value: 0)
+func fetchDataFromServer(completion: @escaping () -> Void) {
+    print("Start fetching data from server")
+    
+    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+        print("Data has been retrieved")
+        completion()
+        semaphore1.signal()
+    }
+}
+
+// DISINI ADA DEATHLOCK BROO, HANG PROGRAMNYA, CEK LAGI TAR YEE
+
+fetchDataFromServer {
+    print("🧠 Update UI after data is complete")
+}
+print("📱 UI can still be used while waiting for data...")
+semaphore1.wait()
